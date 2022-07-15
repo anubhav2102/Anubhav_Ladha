1281cb02fdf36139ac7704cb96b8a9c298317543
anubhav2102 <76780167+anubhav2102@users.noreply.github.com>
2022-07-15 23:38:27 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,10 +10,9 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Anubhav Ladha</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h5 className="text-light">Software Developer</h5>
         <CTA/>
         <HeaderSocials/>
-
         <div className="me">
           <img src={ME} alt="me" />
         </div>
