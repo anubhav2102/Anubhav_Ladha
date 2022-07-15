cc3b18950ef254d60ffd54dc466b51a105640026
anubhav2102 <76780167+anubhav2102@users.noreply.github.com>
2022-07-15 23:39:02 +0530

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {GrInstagram} from 'react-icons/gr'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><FaGithub/></a>
-        <a href="https://instagram.com" target="_blank"><GrInstagram/></a>
+        <a href="https://www.linkedin.com/in/anubhav-ladha-617b1b190/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/anubhav2102" target="_blank"><FaGithub/></a>
+        <a href="https://www.instagram.com/anubhav_2102/" target="_blank"><GrInstagram/></a>
     </div>
   )
 }
